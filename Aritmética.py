5c9d80f86c107bea0e344a51342d4563d5cbb3c1
Diego <126633748+Diego-A-M@users.noreply.github.com>
2023-03-02 09:45:18 -0300

    Code:
    import math
    
    x = 9
    y = 8
    z = x + y
    a = x * y
    b = x/y
    
    
    print("O resultado da adição é {}".format(z))
    print("O resultado da multiplicação é {}".format(a))
    print("O resultado da divisão é {}".format(b))

---
+++ Aritmética.py
@@ -0,0 +1,12 @@
+import math
+
+x = 9
+y = 8
+z = x + y
+a = x * y
+b = x/y
+
+
+print("O resultado da adição é {}".format(z))
+print("O resultado da multiplicação é {}".format(a))
+print("O resultado da divisão é {}".format(b))
