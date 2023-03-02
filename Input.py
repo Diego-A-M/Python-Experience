d038d3c56b3b18167dad6bfa1ad931e02a444a5d
Diego <126633748+Diego-A-M@users.noreply.github.com>
2023-03-02 10:07:45 -0300

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,8 @@
+import math
+#função input
+
+x = input("Digite seu nome: ")
+y = input("Digite sua idade: ")
+z = input("Digite sua altura: ")
+
+print("Olá {}, você tem {} Anos, e tem {} de altura.".format(x,y,z))
