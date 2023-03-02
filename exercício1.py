d16d6e8682d0f886bbf5078e0459022b98435c63
Diego <126633748+Diego-A-M@users.noreply.github.com>
2023-03-02 10:55:31 -0300

    Exercício da danki code

---
+++ exercício1.py
@@ -0,0 +1,13 @@
+#Exercício 01 - Área do retângulo
+
+print("Calcule a área de um retângulo")
+
+print("-------------------------------------")
+
+base = input("Qual o tamanho da base?")
+altura = input("Qual o tamanho da altura do seu retângulo")
+
+area = float(base) * float(altura)
+
+print("A área de seu retângulo se resolve com a formula base * altura, que resulta em uma área de:  {}m".format(area))
+
