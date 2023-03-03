e920139e9bf8dbba80401e82f5b755bb8a75a151
Diego <126633748+Diego-A-M@users.noreply.github.com>
2023-03-03 09:37:31 -0300

    Anotações da aula de for

---
+++ AulaFor.py
@@ -0,0 +1,14 @@
+# Sistema for fazendo uma lista com cada caractere de uma string
+#a = "Marastirela"
+
+#for x in a:
+   #print(x)
+#------------------------------------------------------------------------------------------------------------#
+#Sistema for definindo um ponto de partida e um ponto de encerramento.
+#for x in range(6,9):
+    #print(x)
+#------------------------------------------------------------------------------------------------------------#
+#for x in range(0, 6, 1): #range 6 - Cada número representa algo, o primeiro representa o ponto de início da contagem que sempre será zero, o segundo representa o ponto final da contagem que sempre deve-se diminuir 1 do número final e o último numero é o quantos elementos serão incrementados por vez.
+    #print(x)
+#------------------------------------------------------------------------------------------------------------#
+#Deve-se lembrar de que (For x in range(y) é a mesma coisa que fazer: for(x=0;x<=5;x++)
