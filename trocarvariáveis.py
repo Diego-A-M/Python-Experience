bff5f409c05496eb347ff37fa0f06bfe09f7035d
Diego <126633748+Diego-A-M@users.noreply.github.com>
2023-03-07 10:38:45 -0300

    Trocando variáveis.

---
+++ trocarvariáveis.py
@@ -0,0 +1,14 @@
+#trocar variáveis
+
+x = input("Insira um valor para x: ")
+y = input("Insira um valor para y: ")
+
+""""""
+
+temp = x
+x = y
+y = temp
+
+print("X é {} e Y é {}".format(x,y))
+
+""""""
