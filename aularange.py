744028d76ccebe9c8133b30d10466e823b0c8d85
Diego <126633748+Diego-A-M@users.noreply.github.com>
2023-03-20 08:45:48 -0300

    Pratica com range e dominação de strings

---
+++ aularange.py
@@ -0,0 +1,14 @@
+for x in range(10,-1,-1):
+    print(x)
+
+# ------------------------------------
+#Agora, uma prática de dominação de string
+
+w = "chicago"
+y = "queens"
+
+print(w, end=" ")
+print(y) #Este código está sendo usado para que junte as duas variáveis em uma.
+
+for x in w:
+    print(x, end=" ") #Este código está sendo usado para que crie espaços entre as letras da palavra
